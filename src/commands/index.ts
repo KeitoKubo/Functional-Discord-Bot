cfcb64598b0f9432793586d2f5a1b587bd307e5d
take1-stock <105027725+take1-stock@users.noreply.github.com>
2023-01-22 02:28:55 +0900

    改行した方が見やすいと思う

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,7 +3,9 @@ import timeNotificaiton from "./time";
 import shutDown from "./shutdown";
 import gameInviteEmbed from "./gameInviteEmbed";
 
-// まとめてエクスポート
+// まとめてエクスポート 改行した方が後で管理しやすい
 export default [
-  timeNotificaiton,shutDown,gameInviteEmbed,
+  timeNotificaiton,
+  shutDown,
+  gameInviteEmbed,
 ]
