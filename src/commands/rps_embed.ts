4d5af6e1b07640697491f57bd08b5a651116d9fc
take1-stock <105027725+take1-stock@users.noreply.github.com>
2023-01-23 06:01:41 +0900

    rock_paper_scissors -> rps_embed

--- src/commands/rps_embed.ts
+++ src/commands/rps_embed.ts
@@ -1,7 +1,7 @@
 // typescriptはrequire使ったほうが良い vscodeが型情報をうまく読み取ってくれる
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Events, Message } from "discord.js";
 import emoji from 'node-emoji';
-import config from "./../config/config.json";
+import config from "../config/config.json";
 
 const prefix = config.prefix;
 
