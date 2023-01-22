4e709984a31537b63309d71efba2786efe9d87e0
take1-stock <105027725+take1-stock@users.noreply.github.com>
2023-01-23 06:07:28 +0900

    rps_embedに名前変えた

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,12 +2,14 @@
 import timeNotificaiton from "./time";
 import shutDown from "./shutdown";
 import gameInviteEmbed from "./gameInviteEmbed";
-import rps from "./rps_embed";
+import rps_embed from "./rps_embed";
+import rps_interaction from "./rps_interaction";
 
 // まとめてエクスポート 改行した方が後で管理しやすい
 export default [
   timeNotificaiton,
   shutDown,
   gameInviteEmbed,
-  rps,
+  rps_embed,
+  rps_interaction,
 ]
