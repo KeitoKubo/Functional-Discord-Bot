fe72f496a52d180dc4784399c204cd01f98532ba
take1-stock <105027725+take1-stock@users.noreply.github.com>
2023-01-23 06:05:56 +0900

    rock_paper_scissors -> rps_embed

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,7 +2,7 @@
 import timeNotificaiton from "./time";
 import shutDown from "./shutdown";
 import gameInviteEmbed from "./gameInviteEmbed";
-import rps from "./rock_paper_scissors";
+import rps from "./rps_embed";
 
 // まとめてエクスポート 改行した方が後で管理しやすい
 export default [
